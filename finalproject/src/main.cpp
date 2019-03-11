861869f60bc0bca79486653d524e8f24c4392cf2
Lena Voytek <dvoytek@email.arizona.edu>
2019-03-11 11:59:00 -0700

    Initial

---
+++ finalproject/src/main.cpp
@@ -0,0 +1,20 @@
+// Authors: Nadine Alnajdawi, Jacob Bowles, Jessica Sofka, Lena Voytek    
+// Net IDs: nadinealnajdawi, bowlesj, jsofka, dvoytek      
+// Date: May 2019      
+// Assignment: Final Project
+//
+// Description: This file contains a programmatic overall description of the
+// program. It should never contain assignments to special function registers
+// for the exception key one-line code such as checking the state of the pin.
+//
+
+
+int main()
+{
+    while(1)
+    {
+
+    }
+
+  return 0;
+}
