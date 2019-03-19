3cb303a7efe1d2878933272f4df2d81d74327099
Lena <dvoytek@email.arizona.edu>
2019-03-19 09:14:40 -0700

    Added screen files

---
+++ finalproject/src/screen.cpp
@@ -0,0 +1,18 @@
+// Authors: Nadine Najdawi, Jacob Bowles, Jessica Sofka, Lena Voytek    
+// Net IDs: nadinealnajdawi, bowlesj, jsofka, dvoytek      
+// Date: May 2019      
+// Assignment: Final Project
+
+#include "screen.h"
+
+void initScreen()
+{
+    //Set all query values to zero
+    for(int i = 0; i < NUM_QUERIES; i++)
+        audioQueries[i] = 0;
+}
+
+void displayGraph()
+{
+    
+}
---
+++ finalproject/src/screen.h
@@ -0,0 +1,11 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#define NUM_QUERIES 100
+
+int audioQueries[NUM_QUERIES];
+
+void initScreen();
+void displayGraph();
+
+#endif
