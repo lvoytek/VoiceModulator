aed89bf5c87917242eaef7cb38d3b6e341290309
lvoytek <dvoytek@email.arizona.edu>
2019-04-04 09:03:33 -0700

    Added AudioOut init

--- finalproject/src/audioOut.cpp
+++ finalproject/src/audioOut.cpp
@@ -2,13 +2,16 @@
 // Net IDs: nadinealnajdawi, bowlesj, jsofka, dvoytek      
 // Date: May 2019      
 // Assignment: Final Project
-// NADINE DOES THIS
+// LENA DOES THIS
 
 #include "audioOut.h"
 
+#include <util/delay.h>
+
+/* Initialize I2S Pins as output */
 void initAudioOut()
 {
-
+    DDRB |= (1<<I2S_SCK) | (1<<I2S_WS) | (1<<I2S_SD);
 }
 
 void playAudio(int sample)
--- finalproject/src/audioOut.h
+++ finalproject/src/audioOut.h
@@ -6,6 +6,13 @@
 #ifndef AUDIOOUT_H
 #define AUDIOOUT_H
 
+#include <avr/io.h>
+
+//I2S Pin Definitions
+#define I2S_SCK  PB7
+#define I2S_WS   PB6
+#define I2S_SD   PB5
+
 void initAudioOut();
 
 void playAudio(int sample);
