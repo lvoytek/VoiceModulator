b1b5c0680f3a07801369f1ba32e6a707fad61c89
lvoytek <dvoytek@email.arizona.edu>
2019-04-11 11:24:33 -0700

    Added numpad function overview

--- finalproject/src/numPad.cpp
+++ finalproject/src/numPad.cpp
@@ -6,7 +6,19 @@
 
 #include "numPad.h"
 
+/* Initialize pins 6 - 13 for Numpad Input */
 void initNumPad()
 {
     
+}
+
+/*
+ * Check if a NumPad button has been pressed, if
+ * so return its corresponding numeric value
+ * 
+ * Returns 0 if no button currently pressed
+ */
+int getNumpadValue()
+{
+    return 0;
 }
--- finalproject/src/numPad.h
+++ finalproject/src/numPad.h
@@ -8,5 +8,5 @@
 
 void initNumPad();
 
-
+int getNumpadValue();
 #endif
