42b765752fdf45fbe168d7451fcd236965b047bb
bowlesj1 <48452249+bowlesj1@users.noreply.github.com>
2019-04-15 11:45:33 -0700

    updated

--- finalproject/src/numPad.cpp
+++ finalproject/src/numPad.cpp
@@ -6,19 +6,83 @@
 
 #include "numPad.h"
 
-/* Initialize pins 6 - 13 for Numpad Input */
-void initNumPad()
+#include "NUMPAD.h"
+#include <Keypad.h>
+#include <Key.h>
+
+const byte ROWS = 4; // Four rows
+const byte COLS = 4; // Three columns
+// Define the Keymap
+char hold;//data of key pressed
+void initNumPad(){//startup
+  init();
+setup();
+}
+char keys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte rowPins[ROWS] = {  22,24,26,28};//left most wire on keypad
+// Connect keypad COL0, COL1 and COL2 to these Arduino pins.
+byte colPins[COLS] = { 30,32,34,36 }; 
+
+// Create the Keypad
+Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+
+
+void setup()
 {
-    
+  
+  Serial.begin(9600);
 }
 
-/*
- * Check if a NumPad button has been pressed, if
- * so return its corresponding numeric value
- * 
- * Returns 0 if no button currently pressed
- */
-int getNumpadValue()
+void loop()
 {
-    return 0;
+  char key = kpd.getKey();
+  if(key)  // Check for a valid key.
+  {
+   
+      //  Serial.println(key);
+    hold=key;
+  }
+}
+int getNumpadValue(){//return key
+int tomain;
+switch (hold){//convert to int
+  case '1':
+tomain=1;
+break;
+case '2':
+tomain=2;
+break;
+case '3':
+tomain=3;
+break;
+case'4':
+tomain=4;
+break;
+case '5':
+tomain=5;
+break;
+case '6':
+tomain=6;
+break;
+case '7':
+tomain=7;
+break;
+case'8':
+tomain=8;
+break;
+case '9':
+tomain=9;
+break;
+case '0':
+tomain=0;
+break;
+}
+return tomain;//takes data to main
 }
